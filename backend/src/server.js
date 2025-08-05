8f89262eb6bc89a2a13cd064ecea36b6d1744299
Mohit Kumar <mohit200124.com>
2025-08-05 15:38:23 +0530

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import cors from "cors";   
-import path from "path";
+// import path from "path";
 
 
 import authRoutes from "./routes/auth.route.js";
@@ -11,12 +11,12 @@ import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
 
-dotenv.config(); // Load .env FIRST
+dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-const __dirname =   path.resolve();
+// const __dirname = path.resolve();
 
 app.use(cors({
 origin:  "http://localhost:5173",
@@ -26,19 +26,19 @@ credentials: true
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes); 
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
 
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));  
+// if(process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));  
   
-  app.get("*", (req,res) =>{
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));   
-  })
+//   app.get("*", (req,res) =>{
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));   
+//   })
 
-}
+// }
 
 
 
